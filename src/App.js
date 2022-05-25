f08c8ab874a6a1a079ef8473bcb9378b3506fc16
Sam Shearer <85294886+sshearer101@users.noreply.github.com>
2022-05-25 14:20:53 -0600

    practice

--- src/App.js
+++ src/App.js
@@ -13,32 +13,75 @@ const data = [
   {question: "Can I purchase items again?",
   answer: "dfnjdsnfjkd"}
 ]
-const [selected, setSelected] = useState(null)
 
-const toggle = (i) => {
-  if(selected === i){
-    return setSelected(null)
-  }
-  setSelected(i)
+const [selected, setSelected] = useState()
+
+function toggle(i){
+if (selected === i){
+  return setSelected(null)
+}
+setSelected(i)
 }
 
+
 return(
-  <div>
+<div>
+  {data.map((item, i) => 
     <div>
-    {data.map((item, i) => 
-      <div>
-        <div onClick={() => toggle(i)}>
-         <h1>{item.question}</h1>
-          <span>{selected === i ? '-' : '+'}</span>
-        </div>
-        <div className={selected === i ? "content-show" : "content-hide"}>
-          {item.answer}
-        </div>
+    <div onClick={() => toggle(i)}>
+      <h1>{item.question}</h1>
+      <span>{selected === i ? "-" : "+"}</span>
       </div>
-    )}
+      <div className ={selected === i ? "content-show" : "content-hide"}>
+        {item.answer}
+      </div>
     </div>
-  </div>
+  )}
+
+</div>
+
+
 )
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const [selected, setSelected] = useState()
+
+// function toggle(i){
+//   if(selected === i){
+//     return setSelected(null)
+//   }
+//   setSelected(i)
+// }
+
+// return(
+//   <div>
+//     <div>
+//     {data.map((item, i) => 
+//       <div>
+//         <div onClick={() => toggle(i)}>
+//          <h1>{item.question}</h1>
+//           <span>{selected === i ? '-' : '+'}</span>
+//         </div>
+//         <div className={selected === i ? "content-show" : "content-hide"}>
+//           {item.answer}
+//         </div>
+//       </div>
+//     )}
+//     </div>
+//   </div>
+// )
 
 }
 export default App;
